c56dddd2da533fdfee121690817942e32950cf47
Deevisha Verma <deevishaa.verma@gmail.com>
2017-03-29 15:06:54 +0530

    Update crawlinglink.py

--- crawlinglink.py
+++ crawlinglink.py
@@ -1,13 +1,19 @@
-import requests
 from bs4 import BeautifulSoup
+import urllib
 
-url = "http://economictimes.indiatimes.com/news/defence/armys-daring-surgical-strike-marks-radical-change-in-indias-pakistan-policy/articleshow/54593808.cms"
+f=open("pagelinks.txt","a+")
+baseurl="http://www.bing.com/search?q=site%3Aeconomictimes.indiatimes.com+demonetization&qs=n&form=QBLH&sp=-1&pq=site%3Aec&sc=0-7&sk=&cvid=A935D5643BEE4D7EB355254C270C6898"
+f.write("%s\n" %baseurl)
+r = urllib.urlopen(baseurl).read()
+soup = BeautifulSoup(r,"lxml")
+all_links=soup.find_all("a")
 
-r = requests.get(url)
+lis=[]
 
-soup = BeautifulSoup(r.content,"lxml")
+for link in all_links:
+	lis.append(link.get("href"))
 
-g_data = soup.find_all("div",{"class":"Normal"})
 
-for item in g_data:
-	print(item.text.encode('cp850','replace').decode('cp850'))
+for item in lis[41:45]:
+	f.write("https://www.bing.com"+"%s\n" % item)
+f.close()
